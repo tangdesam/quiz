de24579060c592a22329d8243a3f1bd4309877bc
ilufa <tangdesam@gmail.com>
2022-12-11 19:59:16 +0700

    starter project

---
+++ src/test/java/id/ilufa/quiz/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ilufa.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
