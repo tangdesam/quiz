dceb9c091971fb2b9f8abcc34cf4d94ab895895a
ilufa <tangdesam@gmail.com>
2023-01-01 19:48:02 +0700

    Add Spring Web and create simple About page

---
+++ src/main/java/id/ilufa/quiz/controller/MainController.java
@@ -0,0 +1,12 @@
+package id.ilufa.quiz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+}
